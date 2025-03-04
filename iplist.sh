c27781332d39397e543bdc7c7d23f19c850470d2
Aka <61598472+e703@users.noreply.github.com>
2025-03-04 12:23:03 +0800

    Update iplist.sh

--- iplist.sh
+++ iplist.sh
@@ -6,8 +6,8 @@ curl -L -o routes4.conf https://github.com/e703/nchnroutes-k/releases/download/v
 # https://github.com/haotianlPM/nchnroutes-k/releases/download/v1.0.0/routes4.conf
 
 # 下载 routes6.conf 文件
-# echo "下载 routes6.conf 文件..."
-# curl -L -o routes6.conf https://github.com/haotianlPM/nchnroutes-k/releases/download/v1.0.0/routes6.conf
+echo "下载 routes6.conf 文件..."
+curl -L -o routes6.conf https://github.com/e703/nchnroutes-k/releases/download/v1.0.0/routes6.conf
 
 # 获取文件大小（单位为 KB）
 filesize_routes4=$(stat -c%s routes4.conf)
@@ -18,11 +18,13 @@ filesize_routes4_kb=$((filesize_routes4/1024))
 # 如果两个文件大小都大于 400KB，则将它们复制到 /etc/bird 文件夹下，并执行 birdc configure 命令
 # if [ "$filesize_routes4_kb" -gt "400" ] && [ "$filesize_routes6_kb" -gt "400" ]; then
 if [ "$filesize_routes4_kb" -gt "400" ]; then
-  echo "复制 routes4.conf 文件到 /etc/bird 文件夹..."
-  cp -f routes4.conf /etc/bird/routes4.conf
+  echo "移动 routes4.conf 文件到 /etc/bird 文件夹..."
+#  cp -f routes4.conf /etc/bird/routes4.conf
+mv routes4.conf /etc/bird/routes4.conf
 
-#  echo "复制 routes6.conf 文件到 /etc/bird 文件夹..."
+#  echo "移动 routes6.conf 文件到 /etc/bird 文件夹..."
 #  cp -f routes6.conf /etc/bird/routes6.conf
+mv routes6.conf /etc/bird/routes6.conf
 
   # 循环执行 birdc configure 命令，直到出现 Reconfigured 为止
   echo "执行 birdc configure 命令..."
